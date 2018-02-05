479d0a3924cf8fd8ed49ed82e6736191411cb116
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-02-05 09:22:56 -0500

    a

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -1,9 +1,28 @@
+import java.util.Arrays;
+//Lingli Zou
+//2/1/2017
+//Lab 3.2
 
 public class ArrayMethods2 {
+	
+	public static void main(String[] args)
+	{
+		String[] a = {"cherry", "apple", "banana", "dragonfruit", "eggplant"};
+		String[] b = {"donuts", "chips", "avacado", "biscuits", "eggs"};
+		merge(a, b);
+		
+		int[] list= {5,3,8,6,1,9,2};
+		System.out.println(Arrays.toString(a));
+		
+	}
+	
 	public static String[] merge(String[] list1, String[] list2)
 	{
 		//precondition: both list1 and list2 contains Strings in alphabetical order from a to z 
-		
+		for(int a=0; a<list1.length; a++)
+		{
+			for(int b=0; b<list2.length; a++)
+		}
 		
 		
 		
