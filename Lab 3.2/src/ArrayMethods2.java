bf219da8e4709100d6c2b98d9bd7f6c63e0f896c
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-02-05 09:09:12 -0500

    >:)

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -3,10 +3,24 @@ public class ArrayMethods2 {
 	public static String[] merge(String[] list1, String[] list2)
 	{
 		//precondition: both list1 and list2 contains Strings in alphabetical order from a to z 
+		
+		
+		
+		
+		
+		
+		
+		//Returns: An array of the two lists merged together. The result must be in alphabetical order.
 	}
 	
 	public static int partition(int[] list)
 	{
+		
+		//*Write a method that partitions a list using the first element as the pivot. You should return the
+		//integer position of the pivot in the newly partitioned array. The original array should be rearranged so that
+		//all the elements before the pivot are less than or equal to the pivot and all the elements after the pivot are
+		//greater than the pivot.
+		
 		
 	}
 }
