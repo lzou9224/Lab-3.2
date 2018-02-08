d1a71d24c07302a5e697603eaf9ee02221710029
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-02-08 09:19:48 -0500

    ;)

--- Lab 3.2/src/ArrayMethods2.java
+++ Lab 3.2/src/ArrayMethods2.java
@@ -7,12 +7,27 @@ public class ArrayMethods2 {
 	
 	public static void main(String[] args)
 	{
-		String[] a = {"avacado", "banana", "cherry", "diamond", "ending"};
-		String[] b = {"apple", "biscuits", "candy", "discuits", "eggs"};
-		String[] total= merge(a,b);
+		//testing arrays
+		String[] test1 = {"apple", "cucumber", "microsoft", "zorro"};
+		String[] test2 = {"banana", "cherry", "mahogany", "oreos", "pinata"};
+		int[] test3 = {3,4,2,7,12,22,0,1};
 		
+		//merge test
+		long start= System.nanoTime();
+		String[] mergeResult= merge(test1, test2);
+		long end= System.nanoTime();
+		long time = end - start;
+		System.out.println("Merge test took : " + time + " nanoseconds");
+		System.out.println(Arrays.toString(mergeResult));
 		
-		System.out.println(Arrays.toString(total));
+		start = System.nanoTime();
+		int pivotFinalPos = partition(test3);
+		end = System.nanoTime();
+		time = end - start;
+		System.out.println("Partition test took : " + time + " nanoseconds");
+		System.out.println("Final Pivot Position: " + pivotFinalPos); 
+		System.out.println(Arrays.toString(test3));
+		
 	}
 	
 	public static String[] merge(String[] list1, String[] list2)
